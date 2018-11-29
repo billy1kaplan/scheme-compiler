d349c4f4b917c71fd72dea515a7670a81d20ce8c
Billy Kaplan <billy1kaplan@gmail.com>
2018-11-29 11:17:50 -0500

    make a central utils folder

---
+++ utils/vector.c
@@ -0,0 +1,127 @@
+#include "vector.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Take an already existing Vector as a parameter. Do not create a Vector
+ inside this function. Set its internal memory size to match the value given in
+ the parameter, allocate an appropriately-sized C array to hold the actual data,
+ and initialize the size variable to 0. */
+
+void init(Vector *vector, int memorySize) {
+    vector->array = (char *)malloc(memorySize * sizeof(char));
+    vector->memorySize = memorySize;
+    vector->size = 0;
+}
+
+/* Clears out the elements of the vector */
+void clear(Vector *vector) {
+    for (int i = 0; i < vector->size; i++) {
+        vector->array[i] = '\0';
+    }
+	vector->size = 0;
+}
+
+/* Removes the array associated with the Vector. */
+
+void cleanup(Vector *vector) {
+	vector->memorySize = 0;
+	vector->size = 0;
+    free(vector->array);
+    free(vector);
+}
+
+/* Print out the Vector for debugging purposes. */
+
+void print(Vector *vector) {
+    printf("[");
+    for (int i=0; i < vector->size; i++) {
+        printf("%c, ", vector->array[i]);
+    }
+    printf("]\n");
+}
+
+/* Insert value at location inside the Vector. If the list has items in
+ positions 0 through n-1, you are only permitted to insert new values at
+ locations 0 through n. (This is identical to Java's ArrayList behavior.) If
+ there is already data at the location you are inserting into, insert slides the
+ rest of the data down one position to make room for the new value. Returns 1 if
+ success, and 0 if the location is invalid (less than 0, or greater than the
+ size). Inserting at the very end of the current data (at position equal to
+ size) is fine. All inserts should increase size by 1.
+
+ If the internal array is too small to fit a new value, a new array of twice the
+ size is created with malloc. The values are copied, the Vector is appropriately
+ updated, and the old array is freed.) */
+
+int insert(Vector *vector, int location, char c) {
+    if (location < 0 || location > vector->size) {
+        return 1;
+    } else if (vector->size == vector->memorySize) {
+        int newMemorySize = 2 * vector->memorySize;
+        char* array = (char *)malloc(newMemorySize * sizeof(char));
+
+        for (int i=location-1; i >= 0; i--) {
+            array[i] = vector->array[i];
+        }
+
+        array[location] = c;
+
+        for (int i=vector->size - 1; i >= location; i--) {
+            array[i + 1] = vector->array[i];
+        }
+
+        free(vector->array);
+
+        vector->memorySize = newMemorySize;
+        vector->array = array;
+        vector->size++;
+        return 0;
+    } else {
+        for (int i=vector->size - 1; i >= location; i--) {
+            vector->array[i + 1] = vector->array[i];
+        }
+        vector->array[location] = c;
+        vector->size++;
+        return 0;
+    }
+}
+
+/* inserts a char to the end of the vector */
+int append(Vector *vector, char c) {
+    return insert(vector, vector->size, c);
+}
+
+/* Obtains value at location inside the Vector. Returns 1 if success, and 0 if
+ the location is invalid (less than 0, or greater than or equal to the
+ size). The value itself is returned through the parameter value. */
+
+int get(Vector *vector, int location, char *c) {
+    if (location < 0 || location >= vector->size) {
+        return 1;
+    } else {
+        *c = vector->array[location];
+        return 0;
+    }
+}
+
+/* Deletes value at location inside the Vector.  Returns 1 if success, and 0 if
+ the location is invalid (less than 0, or greater than or equal to the
+ size). When an item is deleted, everything else past it in the array should be
+ slid down. The internal array itself does not need to be compressed, i.e., you
+ don't need to halve it in size if it becomes half full, or anything like
+ that. */
+int delete(Vector *vector, int location) {
+    if (location < 0 || location >= vector->size) {
+        return 1;
+    } else {
+        // If we're at the end of the array, just decrement size
+        // The final value will be ignored / overwritten.
+        for (int i=location; i < vector->size - 1; i++) {
+            vector->array[i] = vector->array[i + 1];
+        }
+
+        vector->size--;
+
+        return 0;
+    }
+}
